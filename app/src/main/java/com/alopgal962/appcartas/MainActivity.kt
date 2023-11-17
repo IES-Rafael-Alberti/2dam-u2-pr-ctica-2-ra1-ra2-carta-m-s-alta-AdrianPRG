8f220ab9690a2f36b32b0103fc0118b2e6c68d90
AdrianPRG <alopgal962@g.educaand.es>
2023-11-17 23:57:49 +0100

    diseño, algo falla que no permite ver mas crta

--- app/src/main/java/com/alopgal962/appcartas/MainActivity.kt
+++ app/src/main/java/com/alopgal962/appcartas/MainActivity.kt
@@ -9,6 +9,7 @@ import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
 import com.alopgal962.appcartas.cass.Baraja
 import com.alopgal962.appcartas.ui.theme.AppcartasTheme
@@ -17,7 +18,6 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            Baraja.crearbaraja()
         }
     }
 }
--- app/src/main/java/com/alopgal962/appcartas/screens/Actividad.kt
+++ app/src/main/java/com/alopgal962/appcartas/screens/Actividad.kt
@@ -1,5 +1,7 @@
 package com.alopgal962.appcartas.screens
 
+import android.annotation.SuppressLint
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
@@ -9,28 +11,51 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.alopgal962.appcartas.cass.Baraja
+
 @Preview
 @Composable
 fun pantalla(){
+    var baraja=Baraja()
+    baraja.crearbaraja()
+    var id by remember {
+        mutableStateOf("")
+    }
     Column(modifier = Modifier
         .fillMaxSize()
         .background(color = Color.LightGray), horizontalAlignment = Alignment.CenterHorizontally, verticalArrangement = Arrangement.Center)
     {
-        Row {
-            Button(onClick = { }) {
+        Row(verticalAlignment = Alignment.CenterVertically, horizontalArrangement = Arrangement.Center) {
+            imagen2(id = id)
+        }
+        Row(modifier = Modifier.padding(top = 25.dp)) {
+            Button(onClick = { id=baraja.damecarta() }) {
                 Text(text = "DAME CARTA")
             }
-            Button(modifier = Modifier.padding(start = 20.dp),onClick = { /*TODO*/ }) {
+            Button(modifier = Modifier.padding(start = 20.dp),onClick = { baraja.barajar() }) {
                 Text(text = "REINICIAR")
             }
         }
     }
 }
 
-
-
+@Composable
+fun imagen2(id: String) {
+    if (id.isNotEmpty()) {
+        val context = LocalContext.current
+        val obtenerid = context.resources.getIdentifier(id, "drawable", context.packageName)
+        val imagen = painterResource(id = obtenerid)
+        Image(painter = imagen, contentDescription = "imagen")
+    }
+}
